a20075fc7403d8d9e110c36c5d21ba7fa0731784
luhverchikv <luhverchikv@icloud.com>
2026-04-06 16:08:30 +0300

    Add files via upload

---
+++ sourse/telegram/filters/__init__.py
@@ -0,0 +1,6 @@
+from .admin import AdminProtectFilter as AdminProtectFilter
+from .chat_type import ChatTypeFilter as ChatTypeFilter
+from .validators import AgeValidator as AgeValidator
+from .validators import PhoneValidator as PhoneValidator
+
+__all__ = ["AdminProtectFilter", "AgeValidator", "ChatTypeFilter", "PhoneValidator"]
---
+++ sourse/telegram/filters/admin_protect.py
@@ -0,0 +1,3 @@
+from .admin import AdminProtectFilter
+
+__all__ = ["AdminProtectFilter"]
